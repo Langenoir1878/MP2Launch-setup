828f003a059854e314ed86062055cd59bd7ac65d
langenoir <Ln1878@langenoirs-MacBook-Pro.local>
2015-11-19 15:47:07 -0600

    updated cloudwatch metric alarm

--- simpleLaunch.sh
+++ simpleLaunch.sh
@@ -13,3 +13,15 @@ echo -e "\nPlease wait for a few minute, creating database : simmoncatdb . . ."
 aws rds wait db-instance-available --db-instance-identifier simmon-the-cat-db
 
 echo -e "\n Finished creating the database."
+
+# db read-only replica
+echo "\nCreating read-only replica "
+for i in {0..7}; do echo -ne '.'; sleep 1; done
+echo -e "\n"
+
+aws rds create-db-instance-read-replica --db-instance-identifier simmon-the-cat-db-read-only --source-db-instance-identifier simmon-the-cat-db --public-accessible
+
+echo "db read-only replica created! "
+
+# cloudwatch, updated Nov 19th, 2015 ---to be added into launch.sh
+# aws cloudwatch put-metric-alarm --metric-name SIMMON-METRIC --alarm-name SIMMON-ALARM --alarm-description "SIMMON-ALARM triggered! " --namespace AWS/EC2 --dimensions Name=SIMMON-AUTO-SCALE,Value=SIMMON-AUTO-SCALE --statistic Average  --metric-name CPUUtilization --comparison-operator GreaterThanOrEqualToThreshold --threshold 30 --period 360 --evaluation-periods 4 -- alarm-actions arn:(----to be added---)
