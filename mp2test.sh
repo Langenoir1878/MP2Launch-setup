3a1a1fc5a3a665e2334a8d7a3f422b3136f278ba
langenoir <Ln1878@langenoirs-MacBook-Pro.local>
2015-11-19 22:19:00 -0600

    test shell

--- mp2test.sh
+++ mp2test.sh
@@ -2,7 +2,7 @@
 # YZln1878
 # Nov 19, 2015
 
-aws sns create-topic --name mp2
+ARN=$(aws sns create-topic --name mp2)
 
 # returned ARN: arn:aws:sns:us-east-1:186069030643:mp2
 
@@ -15,3 +15,14 @@ aws sns create-topic --name mp2
 # Your subscription's id is: 
 # arn:aws:sns:us-east-1:186069030643:mp2:aef1424e-59c6-449f-801b-d0ad1a12ff48
 
+
+aws sns subscribe --topic-arn $Arn --protocol email --notification-endpoint yzhan214@hawk.iit.edu
+
+aws sns set-topic-attributes --topic-arn $Arn --attribute-name mp2-attribute --attribute-value mp2
+
+aws sns publish --topic-arn $Arn --message "testing sns service"
+
+# to do: 
+
+
+ 
